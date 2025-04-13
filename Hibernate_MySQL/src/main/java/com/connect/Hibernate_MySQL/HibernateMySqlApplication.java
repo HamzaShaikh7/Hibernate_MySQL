71d6a6a85bf6c4a874f43bb66571b2aa5e0614f2
Hamza Shaikh <100403237+HamzaShaikh7@users.noreply.github.com>
2025-04-14 02:19:25 +0530

    Add files via upload

---
+++ Hibernate_MySQL/src/main/java/com/connect/Hibernate_MySQL/HibernateMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.connect.Hibernate_MySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateMySqlApplication.class, args);
+	}
+
+}
---
+++ Hibernate_MySQL/src/test/java/com/connect/Hibernate_MySQL/HibernateMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connect.Hibernate_MySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
